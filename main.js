13f195f872105ff1f4739fa2dce7d8128c82ebb0
YuanJia815 <yuan920815@gmail.com>
2026-04-04 16:09:36 +0800

    ...

--- main.js
+++ main.js
@@ -7,9 +7,8 @@ dotenv.config()
 const app = express();
 app.use(express.json());
 
-let client = null;
-
-client = mqtt.connect("mqtts://fb65afa1d6c34fa29ba74f059d62716c.s1.eu.hivemq.cloud", {
+//===================================== MQTT Setup =====================================//
+const client = mqtt.connect(process.env.MQTT_URL, {
   username: process.env.MQTT_USERNAME,
   password: process.env.MQTT_PASSWORD,
   port: 8883,
@@ -18,23 +17,34 @@ client.on("connect", () => {
   console.log("MQTT connected");
   client.publish("gate/control", "open");
 });
+client.on('reconnect', () => {
+  console.log('🔄 reconnecting...');
+});
 client.on("error", (err) => {
   console.log("MQTT error:", err);
 });
-
-// ✅ HTTP API
-app.get('/open', (req, res) => {
-  client.publish('gate/control', 'open');
-  res.send('LED ON');
+//===================================== HTTP API Setup =====================================//
+app.get('/test', (req, res) => {
+  res.send('人生就像泡麵三分鐘熱度然後後悔又開始懷疑');
 });
 
-app.get('/close', (req, res) => {
-  client.publish('gate/control', 'close');
-  res.send('LED OFF');
+app.post('/gate/control', (req, res) => {
+  const { action } = req.body;
+
+  if (!client.connected) {
+    return res.status(500).send('MQTT not connected');
+  }
+
+  if (!["open", "close", "stop"].includes(action)) {
+    return res.status(400).send('Invalid action');
+  }
+
+  client.publish('esp32/control', action);
+  res.send(`Gate ${action}`);
 });
 
-// Render 需要用 PORT
+//===================================== Start Server =====================================//
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
